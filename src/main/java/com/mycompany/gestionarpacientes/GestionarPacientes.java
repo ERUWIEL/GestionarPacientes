da5f7872c9ceaf5e00d27c3247751d5e30bdd7e2
erwbyel <angelerubielflores@gmail.com>
2025-12-06 18:40:37 -0700

    inital commit

---
+++ src/main/java/com/mycompany/gestionarpacientes/GestionarPacientes.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.gestionarpacientes;
+
+/**
+ *
+ * @author gatog
+ */
+public class GestionarPacientes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
